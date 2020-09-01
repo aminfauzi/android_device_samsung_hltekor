31e0c62c2e430e7514fcfb9e33f01c2d0c8ed3c5
Amin Fauzi <aremean0107@gmail.com>
2020-09-01 09:16:35 +0000

    hltekor: Go Resurrection

--- rr_hltekor.mk
+++ rr_hltekor.mk
@@ -13,10 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ResurrectionRemix OS stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/hltekor/full_hlte.mk)
 
 PRODUCT_DEVICE := hltekor
-PRODUCT_NAME := lineage_hltekor
+PRODUCT_NAME := rr_hltekor
